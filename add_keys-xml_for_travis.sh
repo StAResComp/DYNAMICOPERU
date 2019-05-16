4944c78770406de2884e097976949a378b4c0fa0
Paddy McCann <pgm5@st-andrews.ac.uk>
2019-05-16 12:14:51 +0100

    Provides key for travis

--- add_keys-xml_for_travis.sh
+++ add_keys-xml_for_travis.sh
@@ -1,1 +1,1 @@
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><resources><string name=\"google_maps_key\">api-key-goes-here</string></resources>" > catch/src/main/res/values/keys-travis.xml
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><resources><string name=\"google_maps_key\">api-key-goes-here</string></resources>" > app/src/main/res/values/keys-travis.xml
